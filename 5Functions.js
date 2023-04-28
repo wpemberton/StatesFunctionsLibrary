69500089e5cea50e74ce36519418b78e01b88855
wpemberton <116366649+wpemberton@users.noreply.github.com>
2023-04-28 13:29:36 -0400

    Update 5Functions.js

--- 5Functions.js
+++ 5Functions.js
@@ -1,5 +1,31 @@
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Geography/US%20States.csv";
 
+function getColumn(url, columnNumber){
+  var column = [];
+  var table = [];
+  var request = new XMLHttpRequest();  
+  request.open("GET", url, false);   
+  request.send(null);  
+  var csvData = new Array();
+  var jsonObject = request.responseText.split(/\r?\n|\r/);
+  for (var i = 0; i < jsonObject.length; i++) {
+    csvData.push(jsonObject[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/));
+  }
+  table = csvData;
+  column = getCol(table, columnNumber);
+  return column;
+}
+
+//returns the specified column from a 2D Array
+function getCol(matrix, col){
+       var column = [];
+       for(var i=1; i<matrix.length-1; i++){
+          column.push(matrix[i][col]);
+       }
+       return column;
+    }
+
+
 var stateNames = getColumn(url, 1);
 var codeName = getColumn(url, 2);
 var nickname = getColumn(url, 3);
